dd738ae972af7e9bbc231d5d20901126421c4349
Sebastian <sebablasko@gmail.com>
2016-03-22 12:12:26 -0300

    Agrega alarma para gatillar rutina de sincronización. Close #19

---
+++ app/src/main/java/cl/niclabs/adkintunmobile/services/sync/SynchronizationBroadcastReceiver.java
@@ -0,0 +1,27 @@
+package cl.niclabs.adkintunmobile.services.sync;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SynchronizationBroadcastReceiver extends BroadcastReceiver {
+    public SynchronizationBroadcastReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Toast.makeText(context, "Se gatilló", Toast.LENGTH_SHORT).show();
+        context.startService(new Intent(context, Synchronization.class));
+    }
+
+    public void setSchedulle(Context context){
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, SynchronizationBroadcastReceiver.class);
+        PendingIntent pIntent = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        //am.set(AlarmManager.RTC, System.currentTimeMillis() + 5000, pIntent);
+        alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), 15000, pIntent);
+    }
+}
