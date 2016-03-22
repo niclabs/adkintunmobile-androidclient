899fcb45c5117d4e484afb715f616879757bde88
Sebastian <sebablasko@gmail.com>
2016-03-22 15:45:50 -0300

    close #20

---
+++ app/src/main/java/cl/niclabs/adkintunmobile/Constants.java
@@ -0,0 +1,8 @@
+package cl.niclabs.adkintunmobile;
+
+public class Constants {
+
+    public static final String URL_REPORTS = "http://172.30.65.207:5000/";
+    public static final long MILIS_REPEATING_SYNC = 15000;
+
+}
