97765e15b1e7e98657a0d305307f85fec6843e53
Sebastian <sebablasko@gmail.com>
2016-03-22 12:07:15 -0300

    Se agrega una clase EventsReport para facilitar la serialización de los elementos al momento de enviar el reporte en el rpoceso de sincronización. Parte del issue #7 para la rutina de sincronización

---
+++ app/src/main/java/cl/niclabs/adkintunmobile/data/EventsReport.java
@@ -0,0 +1,52 @@
+package cl.niclabs.adkintunmobile.data;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import cl.niclabs.adkintunmobile.data.persistent.CdmaObservationWrapper;
+import cl.niclabs.adkintunmobile.data.persistent.ConnectivityObservationWrapper;
+import cl.niclabs.adkintunmobile.data.persistent.GsmObservationWrapper;
+import cl.niclabs.adkintunmobile.data.persistent.SampleWrapper;
+import cl.niclabs.adkintunmobile.data.persistent.StateChangeWrapper;
+import cl.niclabs.adkintunmobile.data.persistent.TelephonyObservationWrapper;
+import cl.niclabs.adkintunmobile.data.persistent.TrafficObservationWrapper;
+
+public class EventsReport {
+
+    // TODO: Agregar Campos para datos del dispositivo
+    // TODO: Modificar Nombres de los campos Serializados
+
+    @SerializedName("cdma_records")
+    public List <CdmaObservationWrapper> cdmaRecords;
+    @SerializedName("connectivity_records")
+    public List <ConnectivityObservationWrapper> connectivityRecords;
+    @SerializedName("gsm_records")
+    public List <GsmObservationWrapper> gsmRecords;
+    @SerializedName("state_records")
+    public List <StateChangeWrapper> stateRecords;
+    @SerializedName("telephony_records")
+    public List <TelephonyObservationWrapper> telephonyRecords;
+    @SerializedName("traffic_records")
+    public List <TrafficObservationWrapper> trafficRecords;
+
+    public EventsReport() {
+        this.cdmaRecords = CdmaObservationWrapper.listAll(CdmaObservationWrapper.class);
+        this.connectivityRecords = ConnectivityObservationWrapper.listAll(ConnectivityObservationWrapper.class);
+        this.gsmRecords = GsmObservationWrapper.listAll(GsmObservationWrapper.class);
+        this.stateRecords = StateChangeWrapper.listAll(StateChangeWrapper.class);
+        this.telephonyRecords = TelephonyObservationWrapper.listAll(TelephonyObservationWrapper.class);
+        this.trafficRecords = TrafficObservationWrapper.listAll(TrafficObservationWrapper.class);
+    }
+
+    public void cleanDBRecords(){
+        CdmaObservationWrapper.deleteAll(CdmaObservationWrapper.class);
+        GsmObservationWrapper.deleteAll(GsmObservationWrapper.class);
+        SampleWrapper.deleteAll(SampleWrapper.class);
+
+        ConnectivityObservationWrapper.deleteAll(ConnectivityObservationWrapper.class);
+        StateChangeWrapper.deleteAll(StateChangeWrapper.class);
+        TelephonyObservationWrapper.deleteAll(TelephonyObservationWrapper.class);
+        TrafficObservationWrapper.deleteAll(TrafficObservationWrapper.class);
+    }
+}
