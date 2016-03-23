f5dceed449237fd2a5806f02c95f4e3332d99171
Sebastian <sebablasko@gmail.com>
2016-03-23 16:22:02 -0300

    actualizados tiempos de sincronización

--- app/src/main/java/cl/niclabs/adkintunmobile/Constants.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/Constants.java
@@ -3,6 +3,6 @@ package cl.niclabs.adkintunmobile;
 public class Constants {
 
     public static final String URL_REPORTS = "http://172.30.65.207:5000/";
-    public static final long MILIS_REPEATING_SYNC = 15000;
+    public static final long MILIS_REPEATING_SYNC = 1000 * 60;
 
 }
