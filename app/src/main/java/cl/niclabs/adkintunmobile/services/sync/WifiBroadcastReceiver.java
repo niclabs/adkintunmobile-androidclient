d63d8f60b818125fbbda5bbc7b3e12f54a2c766f
Sebastian <sebablasko@gmail.com>
2016-03-24 12:24:29 -0300

    Reactiva la cola de Volley sólo si hay conectividad wifi

--- app/src/main/java/cl/niclabs/adkintunmobile/services/sync/WifiBroadcastReceiver.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/services/sync/WifiBroadcastReceiver.java
@@ -3,11 +3,17 @@ package cl.niclabs.adkintunmobile.services.sync;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 
 import cl.niclabs.adkintunmobile.utils.volley.VolleySingleton;
 
 public class WifiBroadcastReceiver extends BroadcastReceiver{
     public void onReceive(Context context, Intent intent) {
-        VolleySingleton.getInstance(context).getRequestQueue().start();
+        NetworkInfo netInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+
+        if (netInfo != null && netInfo.isConnected()) {
+            VolleySingleton.getInstance(context).getRequestQueue().start();
+        }
     }
 }
