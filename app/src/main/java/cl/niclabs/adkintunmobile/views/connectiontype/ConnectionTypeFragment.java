a1cad44cb11681df5fda6a113651911fb1c2a276
Sebastian <sebablasko@gmail.com>
2016-04-05 17:39:06 -0300

    agrega fragment de connection type

---
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/connectiontype/ConnectionTypeFragment.java
@@ -0,0 +1,31 @@
+package cl.niclabs.adkintunmobile.views.connectiontype;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.niclabs.adkintunmobile.R;
+
+public class ConnectionTypeFragment extends Fragment {
+
+    private String title;
+    private Context context;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.title = getActivity().getString(R.string.view_connection_type);
+        this.context = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_connection_type, container, false);
+        return view;
+    }
+
+}
