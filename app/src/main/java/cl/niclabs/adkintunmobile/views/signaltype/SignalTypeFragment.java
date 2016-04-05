54c84becd607c67043a460e7d475a656cb80027b
Sebastian <sebablasko@gmail.com>
2016-04-05 17:40:01 -0300

    agrega fragment signal type

---
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/signaltype/SignalTypeFragment.java
@@ -0,0 +1,31 @@
+package cl.niclabs.adkintunmobile.views.signaltype;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.niclabs.adkintunmobile.R;
+
+public class SignalTypeFragment extends Fragment {
+
+    private String title;
+    private Context context;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.title = getActivity().getString(R.string.view_signal_type);
+        this.context = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+         View view = inflater.inflate(R.layout.fragment_signal_type, container, false);
+        return view;
+    }
+
+}
