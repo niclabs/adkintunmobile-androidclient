230877f2e7f9bea5f127fe21dbd2806cc6efd8ee
Sebastian <sebablasko@gmail.com>
2016-04-05 17:27:26 -0300

    agrega fragment de estado actual

---
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/status/StatusFragment.java
@@ -0,0 +1,31 @@
+package cl.niclabs.adkintunmobile.views.status;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.niclabs.adkintunmobile.R;
+
+public class StatusFragment extends Fragment {
+
+    private String title;
+    private Context context;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.title = getActivity().getString(R.string.view_status);
+        this.context = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_status, container, false);
+        return view;
+    }
+
+}
