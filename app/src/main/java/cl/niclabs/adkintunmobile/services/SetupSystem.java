2808a8f38438a64b61e1fd7a8676291c51f5691c
Sebastian <sebablasko@gmail.com>
2016-04-08 15:23:55 -0300

    agrega seteo de app_version_code como una shared preference

--- app/src/main/java/cl/niclabs/adkintunmobile/services/SetupSystem.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/services/SetupSystem.java
@@ -13,7 +13,7 @@ import cl.niclabs.adkintunmobile.services.monitors.TrafficMonitor;
 import cl.niclabs.adkintunmobile.services.sync.SynchronizationBroadcastReceiver;
 import cl.niclabs.adkmobile.monitor.Device;
 
-public class SetupSystem extends Device{
+public class SetupSystem extends Device {
 
     static private final String TAG = "AdkM:SetupSystem";
 
@@ -33,7 +33,7 @@ public class SetupSystem extends Device{
      * Static methods for service settings and timers
      */
 
-    static public void startUpSystem(Context context){
+    static public void startUpSystem(Context context) {
 
         // Start Monitoring Services
         SetupSystem.startMonitoringServices(context);
@@ -41,24 +41,27 @@ public class SetupSystem extends Device{
         // Start Broadcast Receivers
         SetupSystem.schedulleBroadcastReceivers(context);
 
+        // Register preferences
+        SetupSystem.setAppVersionCode(context);
+
         Log.d(TAG, "Sistemas reconfigurados");
     }
 
-    static public void startMonitoringServices(Context context){
-        if(!ConnectivityMonitor.isRunning())
+    static public void startMonitoringServices(Context context) {
+        if (!ConnectivityMonitor.isRunning())
             context.startService(new Intent(context, ConnectivityMonitor.class));
-        if(!TelephonyMonitor.isRunning())
+        if (!TelephonyMonitor.isRunning())
             context.startService(new Intent(context, TelephonyMonitor.class));
-        if(!TrafficMonitor.isRunning())
+        if (!TrafficMonitor.isRunning())
             context.startService(new Intent(context, TrafficMonitor.class));
     }
 
     private void bootOffSystem(Context context) {
-        if(ConnectivityMonitor.isRunning())
+        if (ConnectivityMonitor.isRunning())
             context.stopService(new Intent(context, ConnectivityMonitor.class));
-        if(TelephonyMonitor.isRunning())
+        if (TelephonyMonitor.isRunning())
             context.stopService(new Intent(context, TelephonyMonitor.class));
-        if(TrafficMonitor.isRunning())
+        if (TrafficMonitor.isRunning())
             context.stopService(new Intent(context, TrafficMonitor.class));
     }
 
@@ -70,6 +73,12 @@ public class SetupSystem extends Device{
         sync.setSchedule(context, sampleRepeatTime);
     }
 
+    static public void setAppVersionCode(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(context.getString(R.string.settings_app_version_key), context.getString(R.string.settings_app_version_default));
+        editor.apply();
+    }
 
 
 }
