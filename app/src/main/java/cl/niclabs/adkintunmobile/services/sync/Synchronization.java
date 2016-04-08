152054355ae9b758e36a7930432483d27c8062cc
Sebastian <sebablasko@gmail.com>
2016-04-08 15:17:18 -0300

    refactoring de formato

--- app/src/main/java/cl/niclabs/adkintunmobile/services/sync/Synchronization.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/services/sync/Synchronization.java
@@ -20,6 +20,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import cl.niclabs.adkintunmobile.R;
 import cl.niclabs.adkintunmobile.data.Report;
@@ -67,7 +70,7 @@ public class Synchronization extends Service {
     /*
      *  Utility Methods
      */
-    private byte[] collectStoredData(Report report){
+    private byte[] collectStoredData(Report report) {
         // The output object
         byte[] data = null;
 
@@ -85,8 +88,9 @@ public class Synchronization extends Service {
             outStream.close();
             data = readContentIntoByteArray(outputFile);
 
-            // delete cache file
-            outputFile.delete();
+            // Delete cache file
+            if (!outputFile.delete())
+                Log.d(this.TAG, "Error al borrar el archivo temporal");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -95,40 +99,39 @@ public class Synchronization extends Service {
         return data;
     }
 
-    private static byte[] readContentIntoByteArray(File file){
-        FileInputStream fileInputStream = null;
+    private static byte[] readContentIntoByteArray(File file) {
+        FileInputStream fileInputStream;
         byte[] bFile = new byte[(int) file.length()];
-        try{
-            //convert file into array of bytes
+        try {
+            // Convert file into array of bytes
             fileInputStream = new FileInputStream(file);
             fileInputStream.read(bFile);
             fileInputStream.close();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return bFile;
     }
 
-    private void sendData(byte[] data){
+    private void sendData(byte[] data) {
         byte[] multipartBody = null;
 
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(bos);
         try {
-            //primer archivo adjuntado al DataOutputStream
+            // Primer archivo adjuntado al DataOutputStream
             HttpMultipartRequest.buildPart(dos, data, "report.JSON");
-            //agregar "multipart form data" después de los archivos
+            // Agregar "multipart form data" después de los archivos
             HttpMultipartRequest.writeBytes(dos);
-            //crear multipart body
+            // Crear multipart body
             multipartBody = bos.toByteArray();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //creación multipart request
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context);
+        // Creación multipart request
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context);
         HttpMultipartRequest multipartRequest =
                 new HttpMultipartRequest(
                         sharedPreferences.getString(this.context.getString(R.string.settings_sampling_hostname_key), ""),
@@ -139,27 +142,35 @@ public class Synchronization extends Service {
                             public void onResponse(NetworkResponse response) {
                                 Toast.makeText(getApplicationContext(), "Upload successfully!", Toast.LENGTH_SHORT).show();
                                 Log.d(TAG, "Upload successfully!");
-                            }},
+
+                                // Registro de la última sincronización exitosa
+                                // TODO: crear un sistema persistente de BD para almacenar este tipo de eventos
+                                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", java.util.Locale.getDefault());
+                                Date date = new Date();
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString(context.getString(R.string.settings_sampling_lastsync_key), dateFormat.format(date));
+                                editor.apply();
+                            }
+                        },
                         new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
                                 Toast.makeText(getApplicationContext(), "Upload failed!" + error.toString(), Toast.LENGTH_SHORT).show();
-                                Log.d(TAG, "Upload failed! " + error.toString() );
+                                Log.d(TAG, "Upload failed! " + error.toString());
                             }
-                        })
-                {
+                        }) {
                     @Override
                     public void deliverError(VolleyError error) {
                         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(this);
                         VolleySingleton.getInstance(getApplicationContext()).getRequestQueue().stop();
                         Toast.makeText(getApplicationContext(), "Deliver Error, Queued!" + error.toString(), Toast.LENGTH_SHORT).show();
                         Log.d(TAG, "Deliver Error, Queued! " + error.toString());
-                        //mErrorListener.onErrorResponse(error);
+                        // mErrorListener.onErrorResponse(error);
 
                     }
                 };
 
-        //agregar multipartrequest a la cola de peticiones
+        // Agregar multipartrequest a la cola de peticiones
         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(multipartRequest);
     }
 
