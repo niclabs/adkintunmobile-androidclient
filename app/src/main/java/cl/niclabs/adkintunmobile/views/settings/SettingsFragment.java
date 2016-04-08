18fc88fb1924988ed62dbd341a5db1d437561bd3
Sebastian <sebablasko@gmail.com>
2016-04-08 14:55:31 -0300

    Actualiza sistema de configuración de preferencias y de previsualización de las mismas

--- app/src/main/java/cl/niclabs/adkintunmobile/views/settings/SettingsFragment.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/settings/SettingsFragment.java
@@ -4,18 +4,16 @@ package cl.niclabs.adkintunmobile.views.settings;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.Map;
 
 import cl.niclabs.adkintunmobile.R;
 import cl.niclabs.adkintunmobile.services.SetupSystem;
-import cl.niclabs.adkintunmobile.services.sync.SynchronizationBroadcastReceiver;
 
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
 
@@ -36,11 +34,40 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         addPreferencesFromResource(R.xml.preferences);
 
         getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+        updateSummaries();
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        SetupSystem.startUpSystem(this.context);
-        Log.d(TAG, "Cambiadas las preferencias");
+
+        if (key.equals(this.context.getString(R.string.settings_sampling_frequency_key))){
+            SetupSystem.schedulleBroadcastReceivers(this.context);
+        }
+
+        updateSummary(key);
+    }
+
+    private void updateSummaries(){
+        Map<String, ?> prefs = getPreferenceManager().getSharedPreferences().getAll();
+        for (Map.Entry<String, ?> entry : prefs.entrySet()) {
+            // Log.d(TAG,entry.getKey() + ": " + entry.getValue().toString() + " ___ ");
+            updateSummary(entry.getKey());
+        }
+    }
+
+    private void updateSummary(String key){
+        Preference pref = findPreference(key);
+
+        if (pref instanceof ListPreference) {
+            Log.d(TAG, "Cambiada las preferencia de Lista");
+        } else if (pref instanceof EditTextPreference) {
+            EditTextPreference editTextPreference = (EditTextPreference) pref;
+            pref.setSummary(editTextPreference.getText());
+            Log.d(TAG, "Cambiada las preferencia de EditText");
+        } else {
+            pref.setSummary(getPreferenceManager().getSharedPreferences().getString(pref.getKey(), "-"));
+            Log.d(TAG, "Cambiada las preferencia de sistema");
+        }
     }
 }
