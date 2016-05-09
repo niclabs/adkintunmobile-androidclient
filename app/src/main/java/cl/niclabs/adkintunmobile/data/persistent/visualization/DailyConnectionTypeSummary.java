38d32bd1f2ce3563d31dbcbec3879a39099833ed
Diego Madariaga <diego@niclabs.cl>
2016-05-09 16:10:45 -0300

    Se agregan funcionalidades para mostrar resumen en dashboard

--- app/src/main/java/cl/niclabs/adkintunmobile/data/persistent/visualization/DailyConnectionTypeSummary.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/data/persistent/visualization/DailyConnectionTypeSummary.java
@@ -52,6 +52,57 @@ public class DailyConnectionTypeSummary extends Persistent<DailyConnectionTypeSu
         return DailyConnectionTypeSummary.find(DailyConnectionTypeSummary.class, "date = ?", todayWhereArgs, "date").next();
     }
 
+    public static int[] getTimeByTypeSummary(long currentTime){
+        DailyConnectionTypeSummary todaySummary = DailyConnectionTypeSummary.getSummary(currentTime);
+        Iterator<ConnectionTypeSample> todaySamples = todaySummary.getSamples();
+        int[] timeByType = new int[3];
+        long lastTime;
+
+        int lastType;
+        ConnectionTypeSample sample;
+
+        //Info del primer sample del día
+        if (todaySamples.hasNext()){
+            sample = todaySamples.next();
+            lastTime = sample.getInitialTime();
+            lastType = sample.getType();
+        }
+        else {                                      //manejo si no hay valores
+            sample = null;                          //Se podría detectar acá el valor o ejecutar la sincronización
+            lastType = 0;
+            lastTime = currentTime;
+        }
+
+        //Si primer reporte del día no parte de las 0 AM, completar con último del día anterior
+        if (lastTime > currentTime) {
+            DailyConnectionTypeSummary yesterdaySummary = DailyConnectionTypeSummary.getSummary(currentTime - 3600L * 24L * 1000L);
+            Iterator<ConnectionTypeSample> yesterdaySamples = yesterdaySummary.getSamples();
+            if (yesterdaySamples.hasNext()){
+                while (yesterdaySamples.hasNext()) {
+                    sample = yesterdaySamples.next();
+                }
+                lastType = sample.getType();
+            }
+            else {
+                lastType = 0;
+            }
+            timeByType[lastType] += (lastTime - currentTime);
+        }
+
+        //Samples del día seleccionado
+        while (todaySamples.hasNext()){
+            sample = todaySamples.next();
+            if (lastType == sample.getType())
+                continue;
+            timeByType[lastType] += (sample.getInitialTime() - lastTime);
+            lastType = sample.getType();
+            lastTime = sample.getInitialTime();
+        }
+        timeByType[lastType] += (currentTime - lastTime);
+
+        return timeByType;
+    }
+
     public long getDateMillis(){
         return date;
     }
