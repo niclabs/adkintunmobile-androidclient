f10c6cbcb7984b8a8cf59336de717fc5ed232818
Sebastian <sebablasko@gmail.com>
2016-05-11 16:34:45 -0300

    cambios del dialog

--- app/src/main/java/cl/niclabs/adkintunmobile/views/status/StatusSettingsDialog.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/status/StatusSettingsDialog.java
@@ -5,13 +5,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import cl.niclabs.adkintunmobile.R;
 
@@ -31,18 +31,14 @@ public class StatusSettingsDialog extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_status_settings_dialog, container, false);
-        getDialog().setTitle("HOLA!!");
+        getDialog().setTitle(getActivity().getString(R.string.view_status_settings_dialog_title));
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String value = sharedPreferences.getString(getActivity().getString(R.string.settings_app_data_quota_total_key), "0");
         dataQuotaTotalValue = Integer.parseInt(value);
-        Log.d("OK", dataQuotaTotalValue+" "+getActivity().getString(R.string.settings_app_data_quota_total_key));
-        Log.d("OK", sharedPreferences.getAll().toString());
 
-        //mEditText = (EditText) v.findViewById(R.id.txt_your_name);
-        mNumberPicker = (NumberPicker) v.findViewById(R.id.txt_your_name);
+        mNumberPicker = (NumberPicker) v.findViewById(R.id.np_data_quota);
         mButton = (Button) v.findViewById(R.id.bt_save_data_quota);
-
 
         String[] dataQuotaOptions = getResources().getStringArray(R.array.data_quotas);
 
@@ -70,6 +66,7 @@ public class StatusSettingsDialog extends DialogFragment {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(getActivity().getString(R.string.settings_app_data_quota_total_key), Integer.toString(mbDataPlan));
         editor.apply();
+        Toast.makeText(getActivity(), getActivity().getString(R.string.settings_updated), Toast.LENGTH_SHORT).show();
     }
 
 }
