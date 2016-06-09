f18131c0c89a4a3e13dcb821b4293b59909a5b46
Sebastian <sebablasko@gmail.com>
2016-06-09 18:06:17 -0400

    agrega clases de lectura de archivos /proc/net

---
+++ app/src/main/java/cl/niclabs/adkintunmobile/utils/information/SystemSocket.java
@@ -0,0 +1,135 @@
+package cl.niclabs.adkintunmobile.utils.information;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SystemSocket {
+
+    static final private Pattern fieldsPattern = Pattern.compile("^\\s*(\\d+): ([0-9A-F]+):(....) ([0-9A-F]+):(....) (..) (?:\\S+ ){3}\\s*(\\d+)\\s+\\d+\\s+(\\d+).*$");
+
+    //private String slot;
+    private String localAddress;
+    private int localPort;
+    private String remoteAddress;
+    private int remotePort;
+    private String state;
+    private int uid;
+    private String inode;
+
+    SystemSocket(String line) {
+        Matcher match = fieldsPattern.matcher(line);
+        match.lookingAt();
+
+        //slot            = match.group(1);
+        localAddress    = parseIp(match.group(2));
+        localPort       = parsePort(match.group(3));
+        remoteAddress   = parseIp(match.group(4));
+        remotePort      = parsePort(match.group(5));
+        state           = match.group(6);
+        uid             = Integer.parseInt(match.group(7));
+        inode           = match.group(8);
+    }
+
+    private int parsePort(String port){
+        return Integer.parseInt(port, 16);
+    }
+
+    public String getAppName(Context context){
+        String [] packages = context.getPackageManager().getPackagesForUid(uid);
+        String packageName = "", label = "";
+
+
+
+        if (packages != null && packages.length > 0)
+            packageName = packages[0];
+
+        PackageManager pkgManager = context.getPackageManager();
+
+        try {
+            PackageInfo pkgInfo = pkgManager.getPackageInfo(packageName, 0);
+            ApplicationInfo appInfo = pkgInfo.applicationInfo;
+
+            label = pkgManager.getApplicationLabel(appInfo).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            label = packageName;
+        }
+
+        return label;
+    }
+
+    @Override
+    public String toString() {
+        return "SystemSockets.Socket(" +
+                "localAddress = " + localAddress +
+                ", localPort = " + getLocalPort() +
+                ", remoteAddress = " + remoteAddress +
+                ", remotePort = " + getRemotePort() +
+                ", state = " + state +
+                ", uid = " + uid +
+                ", inode = " + inode + ")";
+    }
+
+    public int getRemotePort() {
+        return remotePort;
+    }
+
+    public int getLocalPort() {
+        return localPort;
+    }
+
+    public String getLocalAddress() {
+        return localAddress;
+    }
+
+    public String getRemoteAddress() {
+        return remoteAddress;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public boolean isOutsideConnection(){
+        return !this.remoteAddress.contains("0.0.0.0") &&
+                !this.remoteAddress.contains("127.0.0.1");
+    }
+
+    private String parseIp(String ip){
+        int len = ip.length();
+
+        byte[] retval = new byte[len / 2];
+        for (int i = 0; i < len / 2; i += 4) {
+            retval[i] = (byte) ((Character.digit(ip.charAt(2*i + 6), 16) << 4)
+                    + Character.digit(ip.charAt(2*i + 7), 16));
+            retval[i + 1] = (byte) ((Character.digit(ip.charAt(2*i + 4), 16) << 4)
+                    + Character.digit(ip.charAt(2*i + 5), 16));
+            retval[i + 2] = (byte) ((Character.digit(ip.charAt(2*i + 2), 16) << 4)
+                    + Character.digit(ip.charAt(2*i + 3), 16));
+            retval[i + 3] = (byte) ((Character.digit(ip.charAt(2*i), 16) << 4)
+                    + Character.digit(ip.charAt(2*i + 1), 16));
+        }
+        try {
+            return InetAddress.getByAddress(retval).getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof SystemSocket){
+            return ((SystemSocket) o).getLocalAddress() == this.getLocalAddress() &&
+                    ((SystemSocket) o).getRemoteAddress() == this.getRemoteAddress() &&
+                    ((SystemSocket) o).getUid() == this.getUid();
+        }
+        return super.equals(o);
+    }
+}
---
+++ app/src/main/java/cl/niclabs/adkintunmobile/utils/information/SystemSockets.java
@@ -0,0 +1,92 @@
+package cl.niclabs.adkintunmobile.utils.information;
+
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SystemSockets {
+
+    static final private String TCP_PATH = "/proc/net/tcp";
+    static final private String UDP_PATH = "/proc/net/udp";
+
+    /**
+     * Retorna un ArrayList con obtejos SystemSocket de las conexiones TCP activas
+     * @return
+     */
+    public static ArrayList<SystemSocket> getTCPSockets() {
+        ArrayList<SystemSocket> sockets = new ArrayList<SystemSocket>();
+        parseSocketTable(TCP_PATH, sockets);
+        return sockets;
+    }
+
+    /**
+     * Retorna un ArrayList con obtejos SystemSocket de las conexiones UDP activas
+     * @return
+     */
+    public static ArrayList<SystemSocket> getUDPSockets() {
+        ArrayList<SystemSocket> sockets = new ArrayList<SystemSocket>();
+        parseSocketTable(UDP_PATH, sockets);
+        return sockets;
+    }
+
+
+    /**
+     * Retorna un diccionario con nombres de aplicaciones y numero de conexiones activas en TCP
+     */
+    public static HashMap<String, Integer> getTCPActiveAppsConnections(Context context){
+        HashMap<String, Integer> totals = new HashMap<String, Integer>();
+        for (SystemSocket socket : SystemSockets.getTCPSockets()){
+            if (!totals.containsKey(socket.getAppName(context)))
+                totals.put(socket.getAppName(context), 0);
+            totals.put(socket.getAppName(context), totals.get(socket.getAppName(context)) + 1);
+        }
+        return totals;
+    }
+
+    /**
+     * Retorna un diccionario con nombres de aplicaciones y numero de conexiones activas en UDP
+     */
+    public static HashMap<String, Integer> getUDPActiveAppsConnections(Context context){
+        HashMap<String, Integer> totals = new HashMap<String, Integer>();
+        for (SystemSocket socket : SystemSockets.getUDPSockets()){
+            if (!totals.containsKey(socket.getAppName(context)))
+                totals.put(socket.getAppName(context), 0);
+            totals.put(socket.getAppName(context), totals.get(socket.getAppName(context)) + 1);
+        }
+        return totals;
+    }
+
+    /**
+     *
+     * @param path el path al archivo especifico del directorio /proc/net/ que se desea parsear
+     * @param sockets ArrayList con objetos SystemSocket con información de las conexiones activas
+     */
+    private static void parseSocketTable(String path, ArrayList<SystemSocket> sockets) {
+        String line;
+        try {
+            BufferedReader socketInfoReader = new BufferedReader(new FileReader(path));
+            socketInfoReader.readLine(); // Skip header
+
+            while (( line = socketInfoReader.readLine() ) != null) {
+                SystemSocket currentConnection = new SystemSocket(line);
+                if ( currentConnection.isOutsideConnection() ){
+                    sockets.add(currentConnection);
+                }
+            }
+
+            socketInfoReader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
