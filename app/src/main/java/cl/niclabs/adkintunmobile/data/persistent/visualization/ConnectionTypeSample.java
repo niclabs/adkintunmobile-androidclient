d379fad14e7fce989c94cd2a669e19a327062315
Sebastian <sebablasko@gmail.com>
2016-08-08 16:39:47 -0400

    eliminados setters usados solo por concepto de testing

--- app/src/main/java/cl/niclabs/adkintunmobile/data/persistent/visualization/ConnectionTypeSample.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/data/persistent/visualization/ConnectionTypeSample.java
@@ -3,21 +3,6 @@ package cl.niclabs.adkintunmobile.data.persistent.visualization;
 import cl.niclabs.android.data.Persistent;
 
 public class ConnectionTypeSample extends Persistent<ConnectionTypeSample> {
-    public void setType(int type) {
-        this.type = type;
-    }
-
-    public void setInitialTime(long initialTime) {
-        this.initialTime = initialTime;
-    }
-
-    public DailyConnectionTypeSummary getDate() {
-        return date;
-    }
-
-    public void setDate(DailyConnectionTypeSummary date) {
-        this.date = date;
-    }
 
     protected int type;
     protected long initialTime;
