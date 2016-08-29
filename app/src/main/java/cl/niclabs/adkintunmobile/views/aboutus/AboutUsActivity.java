59b31ed37a0d3b307cee3a19527ef3981a3dc73f
Sebastian <sebablasko@gmail.com>
2016-08-29 17:17:11 -0300

    agrega link al sitio web

--- app/src/main/java/cl/niclabs/adkintunmobile/views/aboutus/AboutUsActivity.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/aboutus/AboutUsActivity.java
@@ -1,7 +1,10 @@
 package cl.niclabs.adkintunmobile.views.aboutus;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import cl.niclabs.adkintunmobile.R;
 
@@ -11,5 +14,10 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+    }
+
+    public void openHomeWebPage(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://adkintunmobile.cl"));
+        startActivity(browserIntent);
     }
 }
