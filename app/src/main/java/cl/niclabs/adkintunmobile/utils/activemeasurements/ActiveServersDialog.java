f9e159c3641695ec0ed01ff02a1d691e435b0b1a
sebablasko <sebablasko@gmail.com>
2016-12-02 15:06:16 -0300

    Cambia string de confirmación por el provisto por el sistema android

--- app/src/main/java/cl/niclabs/adkintunmobile/utils/activemeasurements/ActiveServersDialog.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/utils/activemeasurements/ActiveServersDialog.java
@@ -43,7 +43,7 @@ public class ActiveServersDialog extends DialogFragment {
                             }
                         }
                     );
-        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
