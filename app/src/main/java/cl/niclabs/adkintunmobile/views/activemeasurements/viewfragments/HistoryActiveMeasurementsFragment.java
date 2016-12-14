a4a67d072a2a03bb1370a3b9d1a72249ad546a8c
sebastian <sebablasko@gmail.com>
2016-12-14 17:18:24 -0300

    fragment de obtención de arrays para popular historial de reportes viejos

--- app/src/main/java/cl/niclabs/adkintunmobile/views/activemeasurements/viewfragments/HistoryActiveMeasurementsFragment.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/activemeasurements/viewfragments/HistoryActiveMeasurementsFragment.java
@@ -6,26 +6,26 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import cl.niclabs.adkintunmobile.R;
+import cl.niclabs.adkintunmobile.data.persistent.activemeasurement.MediaTestReport;
 import cl.niclabs.adkintunmobile.views.activemeasurements.ConnectivityTestReportDialog;
 import cl.niclabs.adkintunmobile.views.activemeasurements.MediaTestReportDialog;
 import cl.niclabs.adkintunmobile.views.activemeasurements.SpeedTestReportDialog;
 
 public class HistoryActiveMeasurementsFragment extends ListFragment implements AdapterView.OnItemClickListener {
 
+    public ArrayList<String> reportsDatetimeList, reportTimestampList;
 
     public HistoryActiveMeasurementsFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,32 +38,29 @@ public class HistoryActiveMeasurementsFragment extends ListFragment implements A
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ArrayAdapter adapter;
-
         String measurementsType = getActivity().getIntent().getStringExtra(getString(R.string.settings_active_measurements_key));
-        if (measurementsType.equals(getString(R.string.settings_speed_test_category_key)))
-            adapter = ArrayAdapter.createFromResource(getActivity(),
-                    R.array.sync_sampling_compression_type, android.R.layout.simple_list_item_1);
-        else if (measurementsType.equals(getString(R.string.settings_video_test_category_key)))
-            adapter = ArrayAdapter.createFromResource(getActivity(),
-                    R.array.mobile_operators, android.R.layout.simple_list_item_1);
-        //if (measurementsType.equals(getString(R.string.settings_connectivity_test_category_key)))
-        else
-            adapter = ArrayAdapter.createFromResource(getActivity(),
-                    R.array.data_quotas, android.R.layout.simple_list_item_1);
+        if (measurementsType.equals(getString(R.string.settings_speed_test_category_key))) {
+            // TODO implementar!
+        }else if (measurementsType.equals(getString(R.string.settings_video_test_category_key))) {
+            reportTimestampList = MediaTestReport.getTimestampsAllReports();
+            reportsDatetimeList = MediaTestReport.getDatetimeAllReports();
+        }else {         //if (measurementsType.equals(getString(R.string.settings_connectivity_test_category_key)))
+            // TODO implementar!
+        }
 
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, reportsDatetimeList);
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        showDialog(position);
+        showDialog(parent.getItemAtPosition(position).toString(), position);
     }
 
-    private void showDialog(int position) {
+    private void showDialog(String data, int position) {
         FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
-        Fragment prev = getActivity().getSupportFragmentManager().findFragmentByTag("TEST");
+        Fragment prev = getActivity().getSupportFragmentManager().findFragmentByTag("reportsFragment");
         if (prev != null) {
             ft.remove(prev);
         }
@@ -80,10 +77,11 @@ public class HistoryActiveMeasurementsFragment extends ListFragment implements A
 
 
         Bundle bundle = new Bundle();
-        bundle.putString("value", "una fecha");
-        bundle.putInt("position", position);
+        //bundle.putString("value", data);
+        bundle.putString("value", reportTimestampList.get(position));
+        bundle.putInt("position", 1);
 
         reportsFragment.setArguments(bundle);
-        reportsFragment.show(ft, "TEST");
+        reportsFragment.show(ft, "reportsFragmento");
     }
 }
