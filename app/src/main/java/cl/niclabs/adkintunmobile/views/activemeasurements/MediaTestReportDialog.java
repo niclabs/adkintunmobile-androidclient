b425bcf73fbb7ab8e83c8b6015575ca14bb29416
Diego Madariaga <diego@niclabs.cl>
2017-06-06 10:59:10 -0400

    Change "doughnut_no_info" color

--- app/src/main/java/cl/niclabs/adkintunmobile/views/activemeasurements/MediaTestReportDialog.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/activemeasurements/MediaTestReportDialog.java
@@ -156,7 +156,7 @@ public class MediaTestReportDialog extends DialogFragment {
 
     public void addTableSeparation(){
         View separator = new View(getContext());
-        separator.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.doughnut_no_info));
+        separator.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.light_gray));
         int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics());
         TableLayout.LayoutParams params = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, height);
         params.setMargins(0, 20, 0, 20);
