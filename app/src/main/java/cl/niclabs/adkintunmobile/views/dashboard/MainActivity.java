16881652e7e6bd9c893e1778feeb70f23df7577f
Diego Madariaga <diego@niclabs.cl>
2017-06-13 12:12:00 -0400

    Remove NavigationDrawer

--- app/src/main/java/cl/niclabs/adkintunmobile/views/dashboard/MainActivity.java
+++ app/src/main/java/cl/niclabs/adkintunmobile/views/dashboard/MainActivity.java
@@ -51,12 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
     private Context context;
 
-    // Navigation drawer components
-    private DrawerLayout mDrawer;
-    //private Toolbar toolbar;
-    private NavigationView nvDrawer;
-    //private ActionBarDrawerToggle drawerToggle;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,9 +63,6 @@ public class MainActivity extends AppCompatActivity {
         // Start System
         SetupSystem.startUpSystem(this.context);
 
-        // SetupToolBar();
-        setupNavigationDrawer();
-
         // Initial Fragment: DashboardFragment
         DashboardFragment mDashboardFragment;
         if (savedInstanceState != null){
@@ -83,92 +74,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Show tutorial
         showTutorial();
-    }
-
-    /*
-    private void setupToolBar() {
-        // Setup toolbar as an actionbar
-        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
-    */
-
-    private void setupNavigationDrawer() {
-        // Setup general Layout: actionbar + main view + navigation drawer
-        this.mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        // Setup animation of fade in/out nv_drawer
-        //this.drawerToggle = setupDrawerToggle();
-        //this.mDrawer.setDrawerListener(this.drawerToggle);
-
-        // Setup Navigation Drawer
-        this.nvDrawer = (NavigationView) findViewById(R.id.navigationView);
-        setupDrawerContent(nvDrawer);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawer.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /*
-    private ActionBarDrawerToggle setupDrawerToggle() {
-        return new ActionBarDrawerToggle(this, this.mDrawer, this.toolbar, R.string.drawer_open,  R.string.drawer_close);
-    }
-    */
-
-    public void setupDrawerContent(NavigationView nv) {
-        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-
-                Intent myIntent;
-                switch (item.getItemId()) {
-                    case R.id.nav_dashboard:
-                        updateMainFragment(new DashboardFragment());
-                        break;
-                    case R.id.nav_notifications_log:
-                        openNotificationView(null);
-                        break;
-                    case R.id.nav_status:
-                        openStatusView(null);
-                        break;
-                    case R.id.nav_connection_mode:
-                        openConnectionModeView(null);
-                        break;
-                    case R.id.nav_network_type:
-                        openNetworkTypeView(null);
-                        break;
-                    case R.id.nav_carrier_ranking:
-                        updateMainFragment(new RankingFragment());
-                        break;
-                    case R.id.nav_applications_traffic:
-                        openApplicationTrafficView(null);
-                        break;
-                    case R.id.nav_active_connections:
-                        openActiveConnectionsView(null);
-                        break;
-                    case R.id.nav_active_measurements:
-                        openActiveMeasurementsView(null);
-                        break;
-                    case R.id.nav_settings:
-                        myIntent = new Intent(getApplicationContext(), SettingsActivity.class);
-                        startActivity(myIntent);
-                        break;
-                    case R.id.nav_about_us:
-                        myIntent = new Intent(getApplicationContext(), AboutUsActivity.class);
-                        startActivity(myIntent);
-                        break;
-                }
-                mDrawer.closeDrawers();
-                return true;
-            }
-        });
     }
 
     /***
@@ -213,21 +118,6 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(getApplicationContext(), SettingsActivity.class);
         startActivity(myIntent);
     }
-    /*
-     * Navigation Drawer Synchronization methods
-     */
-    /*
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        this.drawerToggle.syncState();
-    }
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        this.drawerToggle.onConfigurationChanged(newConfig);
-    }
-    */
 
     /*
      * changeCurrentFragment Methods
@@ -306,18 +196,6 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case 7:
-                        mDrawer.openDrawer(GravityCompat.START);
-                        Display display = getWindowManager().getDefaultDisplay();
-                        Point pointTarget = new Point();
-
-                        display.getSize(pointTarget);
-                        pointTarget.y = (int) getResources().getDimension(R.dimen.extended_toolbar_dashboard_height);
-                        pointTarget.x = 0;
-                        mTarget = new PointTarget(pointTarget);
-                        break;
-
-                    case 8:
-                        mDrawer.closeDrawers();
                         mTarget = new ViewTarget(findViewById(R.id.iv_collapsable_toolbar_app_icon));
                         showcaseView.setButtonText(getString(R.string.tutorial_close));
                         break;
